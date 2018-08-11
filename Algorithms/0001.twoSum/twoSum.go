31d7d73b32e839b996de974068ff025b54708ded
LLLeon <jiachenhui1989@gmail.com>
2018-08-11 18:06:55 +0800

    0001.twoSumOnePass

--- Algorithms/0001.twoSum/twoSum.go
+++ Algorithms/0001.twoSum/twoSum.go
@@ -34,13 +34,36 @@ func twoSumTwoPass(nums []int, target int) []int {
 	return nil
 }
 
+func twoSumOnePass(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		complement := target - nums[i]
+
+		// 进行匹配时，第 i 个元素还没有放入 map 中，
+		// 无需限制 i != index
+		if index, ok := m[complement]; ok {
+
+			// 因为刚开始遍历时还没有元素放入 map，是后面的 nums[i]
+			// 跟前面放入 map 中的值匹配，所以一定是 i > index
+			return []int{index, i}
+		}
+
+		m[v] = i
+	}
+
+	return nil
+}
+
 func main() {
-	nums := []int{1, 2, 4, 15, 7}
-	target := 19
+	nums := []int{1, 4, 2, 15}
+	target := 6
 
-	r := twoSum(nums, target)
-	r2 := twoSumMap(nums, target)
+	r1 := twoSum(nums, target)
+	r2 := twoSumTwoPass(nums, target)
+	r3 := twoSumOnePass(nums, target)
 
-	fmt.Println(r)
+	fmt.Println(r1)
 	fmt.Println(r2)
+	fmt.Println(r3)
 }
