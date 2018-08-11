6ed396b146416cb315cddf7d586f967ded56b5dd
LLLeon <jiachenhui1989@gmail.com>
2018-08-11 17:13:39 +0800

    0001.twoSumTwoPass

--- Algorithms/0001.twoSum/twoSum.go
+++ Algorithms/0001.twoSum/twoSum.go
@@ -5,27 +5,42 @@ import (
 )
 
 func twoSum(nums []int, target int) []int {
-	result := make([]int, 2)
-
 	for i, _ := range nums {
 		for t := i + 1; t < len(nums); t++ {
 			if nums[i]+nums[t] == target {
-				result[0] = i
-				result[1] = t
-
-				break
+				return []int{i, t}
 			}
 		}
 	}
 
-	return result
+	return nil
+}
+
+func twoSumTwoPass(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		m[v] = i
+	}
+
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+
+		if v, ok := m[complement]; ok && v != i {
+			return []int{i, v}
+		}
+	}
+
+	return nil
 }
 
 func main() {
-	nums := []int{1, 2, 11, 15, 7}
-	target := 9
+	nums := []int{1, 2, 4, 15, 7}
+	target := 19
 
 	r := twoSum(nums, target)
+	r2 := twoSumMap(nums, target)
 
 	fmt.Println(r)
+	fmt.Println(r2)
 }
