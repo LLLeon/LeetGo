9d94c2fad50a5bd3d6dd086280436b84fa57d2fd
LLLeon <jiachenhui1989@gmail.com>
2018-08-22 21:46:55 +0800

    0003: add slidingWindow func

--- Algorithms/0003.lengthOfLongestSubstr/README.md
+++ Algorithms/0003.lengthOfLongestSubstr/README.md
@@ -12,7 +12,11 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3.Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 ## 解答
-
-先考虑暴力法，枚举出所有子字符串，找到最大的无重复字符的子字符串长度即可。
+### 暴力法
+枚举出所有子字符串，找到最大的无重复字符的子字符串长度即可。
 需要一个函数 IsAllUnique() 来判断子字符串中是否有重复字符，可利用 map 的 key 不可重复特性来记录遍历的字符。
 如果此函数返回 true，则更新无重复字符的子字符串的最大长度。
+
+### 滑动窗口
+可用 map 作为滑动窗口来存不重复的字符，即窗口 [i, j) 中不包含重复字符，时间复杂度为 O(1)。
+这样只要向右滑动索引 j，直到检查到字符 `s[j]` 存在于窗口中，再向右滑动索引 i，直到遍历完所有的 i。
--- Algorithms/0003.lengthOfLongestSubstr/lengthOfLongestSubstr.go
+++ Algorithms/0003.lengthOfLongestSubstr/lengthOfLongestSubstr.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-// 暴力法
+/*
+  暴力法
+*/
+
 func LengthOfLongestSubstr(s string) int {
 	resp := 0
 	n := len(s)
@@ -39,8 +43,44 @@ func IsAllUnique(s string, start, end int) bool {
 	return true
 }
 
-func main() {
-	s := "hello"
+/*
+  滑动窗口
+*/
 
+func SlidingWindow(s string) int {
+	var i, j, resp int
+
+	n := len(s)
+	unique := make(map[uint8]int)
+
+	for i < n && j < n {
+		if _, ok := unique[s[j]]; !ok {
+			unique[s[j]] = j
+			j++
+			length := j - i
+
+			if length > resp {
+				resp = length
+			}
+		} else {
+			delete(unique, s[i])
+			i++
+		}
+	}
+
+	return resp
+}
+
+func main() {
+	s := "abcabcdabc"
+
+	nowStart1 := time.Now()
 	fmt.Println(LengthOfLongestSubstr(s))
+	nowEnd1 := time.Now()
+	fmt.Println(nowEnd1.Sub(nowStart1))
+
+	nowStart2 := time.Now()
+	fmt.Println(SlidingWindow(s))
+	nowEnd2 := time.Now()
+	fmt.Println(nowEnd2.Sub(nowStart2))
 }
