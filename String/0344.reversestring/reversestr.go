8289c0e284c44fb9572e4271568e147f77176d18
LLLeon <jiachenhui1989@gmail.com>
2019-03-26 21:19:05 +0800

    0344: update readme

--- String/0344.reversestring/reversestr.go
+++ String/0344.reversestring/reversestr.go
@@ -1,8 +1,13 @@
 package reversestring
 
 func ReverseString(s []byte) {
+	n := len(s)
+	if n <= 1 {
+		return
+	}
+
 	head := 0
-	tail := len(s) - 1
+	tail := n - 1
 
 	for head < tail {
 		s[head], s[tail] = s[tail], s[head]
