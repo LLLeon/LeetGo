a69fd13f1126bf7507fa0cc18377233dd22a6799
LLLeon <jiachenhui1989@gmail.com>
2021-04-15 02:35:13 +0800

    0124. update annotation

--- tree/0124.maxPathSum/maxPathSum.go
+++ tree/0124.maxPathSum/maxPathSum.go
@@ -21,11 +21,12 @@ func MaxPathSum(root *TreeNode) int {
 		lSum := dfs(root.Left)
 		rSum := dfs(root.Right)
 
-		innerMaxSum := lSum + root.Val + rSum
+		innerMaxSum := lSum + root.Val + rSum // 这是当前递归到的节点与其左右节点各自路径和的和, 也就是当前节点的最终路径和
 		maxSum = max(maxSum, innerMaxSum)
 
-		// 返回的是 (root + 左子树或右子树的和) 或 0, 而左子树或右子树的值也是这么递归得来的,
-		// 所以如此回溯上去, 每次递归都记录当前 root 节点和其左右节点各自路径和的和, 并将其与 maxSum 的较大者作为最终路径和
+		// 递归函数返回的是 (root + 左子树或右子树路径和) 或 0, 而左子树或右子树的路径和也是这么递归得来的,
+		// 所以如此回溯上去, 每次递归都记录当前 root 节点和其左右节点各自路径和的和 innerMaxSum, 并将其与 maxSum 的较大者作为当前节点的最终路径和,
+		// 最终递归完毕, 得到的就是要求的最大路径和
 		res := root.Val + max(lSum, rSum)
 		return max(0, res)
 	}
