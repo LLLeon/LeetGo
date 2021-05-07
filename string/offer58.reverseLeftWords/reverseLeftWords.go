a4a8e441c63c59f49a67f0517df97570518f2d02
LLLeon <jiachenhui1989@gmail.com>
2021-05-07 23:57:28 +0800

    offer58. add ReverseLeftWordsII

--- string/offer58.reverseLeftWords/reverseLeftWords.go
+++ string/offer58.reverseLeftWords/reverseLeftWords.go
@@ -14,3 +14,18 @@ func ReverseLeftWords(s string, n int) string {
 
 	return right + left
 }
+
+func ReverseLeftWordsII(s string, n int) string {
+	if len(s) == 0 || n <= 0 {
+		return s
+	}
+
+	if len(s) < n {
+		return s
+	}
+
+	orgLen := len(s)
+	s += s
+
+	return s[n : orgLen+n]
+}
