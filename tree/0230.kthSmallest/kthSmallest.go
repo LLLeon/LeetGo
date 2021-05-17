806213d560abd8a746a357dea9f0e0523f9d7c53
LLLeon <jiachenhui1989@gmail.com>
2021-05-17 19:52:29 +0800

    0230. add KthSmallestII

--- tree/0230.kthSmallest/kthSmallest.go
+++ tree/0230.kthSmallest/kthSmallest.go
@@ -6,6 +6,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 时间复杂度 O(N), 空间复杂度 O(N).
 func KthSmallest(root *TreeNode, k int) int {
 	res := make([]int, 0)
 	traverse(root, &res)
@@ -20,4 +21,26 @@ func traverse(root *TreeNode, res *[]int) {
 	traverse(root.Left, res)
 	*res = append(*res, root.Val)
 	traverse(root.Right, res)
+}
+
+// 时间复杂度 O(N), 空间复杂度 O(1).
+func KthSmallestII(root *TreeNode, k int) int {
+	res, rank := 0, 0
+	inorder(root, k, &rank, &res)
+
+	return res
+}
+
+func inorder(root *TreeNode, k int, rank *int, res *int) {
+	if root == nil {
+		return
+	}
+
+	inorder(root.Left, k, rank, res)
+	*rank++
+	if *rank == k {
+		*res = root.Val
+		return
+	}
+	inorder(root.Right, k, rank, res)
 }
