6ab146cf73e367b2cb278469a5f04c6fad9dabe6
LLLeon <jiachenhui1989@gmail.com>
2021-05-19 00:04:36 +0800

    0538. update README

--- tree/0538.convertBST/convertBST.go
+++ tree/0538.convertBST/convertBST.go
@@ -18,7 +18,8 @@ func traverse(root *TreeNode, sum *int) {
 		return
 	}
 
-	// 依然是中序遍历, 不过是先遍历右子树, 降序遍历, 这样就可以保证必然是从大到小遍历节点, 累加和即可
+	// 依然是中序遍历, 不过是先遍历右子树, 降序遍历, 这样就可以保证必然是从大到小遍历节点, 累加和即可.
+	// 注意 sum 需要传指针, 因为先遍历的是值较大的节点, 前面的累加和要传递个后面遍历的节点.
 	traverse(root.Right, sum)
 	*sum += root.Val
 	root.Val = *sum
