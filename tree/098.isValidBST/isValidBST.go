18c1589986fcce15e19d1682aadf9fc8271fedea
LLLeon <jiachenhui1989@gmail.com>
2021-05-21 00:35:52 +0800

    098. add IsValidBSTII

--- tree/098.isValidBST/isValidBST.go
+++ tree/098.isValidBST/isValidBST.go
@@ -27,3 +27,29 @@ func helper(root *TreeNode, lower, upper int) bool {
 	// 限定左子树的最大值为 root.Val, 右子树的最小值为 root.Val, 递归判断
 	return helper(root.Left, lower, root.Val) && helper(root.Right, root.Val, upper)
 }
+
+// 时间复杂度 O(N), 空间复杂度 O(N), N 为二叉树节点数量.
+func IsValidBSTII(root *TreeNode) bool {
+	prev := math.MinInt64
+	return traverse(root, &prev)
+}
+
+func traverse(root *TreeNode, prev *int) bool {
+	if root == nil {
+		return true
+	}
+
+	// 如果左子树不满足条件, 直接返回 false
+	if !traverse(root.Left, prev) {
+		return false
+	}
+	// 如果当前节点小于它的左节点, 直接返回 false
+	if root.Val <= *prev {
+		return false
+	}
+	// 如果满足条件, 将当前节点作为 prev 继续遍历
+	*prev = root.Val
+
+	// 同样判断右子树
+	return traverse(root.Right, prev)
+}
