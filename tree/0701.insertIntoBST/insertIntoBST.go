a37ee772435fdd8cbd0a784da3431ff6a4b36d18
LLLeon <jiachenhui1989@gmail.com>
2021-05-22 22:03:59 +0800

    0701. add annotation

--- tree/0701.insertIntoBST/insertIntoBST.go
+++ tree/0701.insertIntoBST/insertIntoBST.go
@@ -12,6 +12,9 @@ func InsertIntoBST(root *TreeNode, val int) *TreeNode {
 	}
 
 	if val < root.Val {
+		// 此时 val 小于 root 的值, 将其插入 root 的左子树,
+		// 而这里 InsertIntoBST() 返回的是将 val 插入 root 的左子树之后返回的根节点,
+		// 将其作为 root 的左节点.
 		root.Left = InsertIntoBST(root.Left, val)
 	}
 
