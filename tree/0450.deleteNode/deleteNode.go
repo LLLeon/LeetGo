8c4e9f34b72cc755a621c2ee103126e1f133017c
LLLeon <jiachenhui1989@gmail.com>
2021-05-22 23:15:21 +0800

    0450. add DeleteNode

---
+++ tree/0450.deleteNode/deleteNode.go
@@ -0,0 +1,58 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func DeleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	// 找到 key
+	if key == root.Val {
+		// 1) root 的左右节点都是 nil
+		if root.Left == nil && root.Right == nil {
+			return nil
+		}
+		// 2) root 的左节点是 nil, 用其右节点替换到自己的位置
+		if root.Left == nil {
+			return root.Right
+		}
+		// 3) root 的右节点是 nil, 用其左节点替换到自己的位置
+		if root.Right == nil {
+			return root.Left
+		}
+		// 4) root 的左右节点都不为 nil, 用其左子树中的最大节点或右子树中的最小节点替换自己的位置
+		if root.Left != nil && root.Right != nil {
+			maxNode := getMaxNode(root.Left)
+			root.Val = maxNode.Val
+			// 删除左子树中的最大节点
+			root.Left = DeleteNode(root.Left, maxNode.Val)
+		}
+	}
+
+	if key < root.Val {
+		root.Left = DeleteNode(root.Left, key)
+	}
+
+	if key > root.Val {
+		root.Right = DeleteNode(root.Right, key)
+	}
+
+	return root
+}
+
+func getMaxNode(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	for root.Right != nil {
+		root = root.Right
+	}
+
+	return root
+}
