4dd62a381636a7a6d672a5534940ce7e76553b63
LLLeon <jiachenhui1989@gmail.com>
2021-05-31 23:43:31 +0800

    094. add InorderTraversal

--- tree/0094.bTreeInorderTraversal/inorderTraversal.go
+++ tree/0094.bTreeInorderTraversal/inorderTraversal.go
@@ -1,1 +1,24 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func InorderTraversal(root *TreeNode) []int {
 	if root == nil {
+		return nil
+	}
+
+	var res []int
+
+	left := InorderTraversal(root.Left)
+	right := InorderTraversal(root.Right)
+
+	res = append(res, left...)
+	res = append(res, root.Val)
+	res = append(res, right...)
+
+	return res
+}
