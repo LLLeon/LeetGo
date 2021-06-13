0fc6d10da50e4a3a37e097336eb407f161878fa5
LLLeon <jiachenhui1989@gmail.com>
2021-06-13 23:10:13 +0800

    0226. add InvertTreeII

--- tree/0226.invertTree/invertTree.go
+++ tree/0226.invertTree/invertTree.go
@@ -17,3 +17,31 @@ func InvertTree(root *TreeNode) *TreeNode {
 
 	return root
 }
+
+// 利用栈来暂存节点的子节点后, 交换子节点位置.
+func InvertTreeII(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	stack := []*TreeNode{root}
+
+	for len(stack) != 0 {
+		// 取出栈顶节点
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// 将该节点的左右子节点入栈
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+
+		// 交换该节点的左右子节点
+		node.Left, node.Right = node.Right, node.Left
+	}
+
+	return root
+}
