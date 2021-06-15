22474f50da41a092e2eedea8357d8e27c4d01a05
LLLeon <jiachenhui1989@gmail.com>
2021-06-16 00:14:54 +0800

    0496. add annotation

--- stack/0496.nextGreaterElement/nextGreaterElement.go
+++ stack/0496.nextGreaterElement/nextGreaterElement.go
@@ -13,15 +13,18 @@ func NextGreaterElement(nums1, nums2 []int) []int {
 		curr1 := nums1[i]
 		j := 0
 
+		// 找到 nums2 中等于 nums[i] 的值的索引
 		for j < len2 && nums2[j] != curr1 {
 			j++
 		}
 
+		// 找到 nums2 中大于 nums[i] 的值 (由于题目说明 nums1 是 nums2 的子集, 所以肯定有相同的值)
 		j++
 		for j < len2 && nums2[j] < curr1 {
 			j++
 		}
 
+		// nums2 遍历完后没有找到满足条件的值
 		if j == len2 {
 			res[i] = -1
 			continue
