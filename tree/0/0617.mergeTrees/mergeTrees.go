11b5bac599423269250368c36e34c967610c3902
LLLeon <jiachenhui1989@gmail.com>
2021-06-21 23:38:43 +0800

    0617. add MergeTrees

---
+++ tree/0/0617.mergeTrees/mergeTrees.go
@@ -0,0 +1,27 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func MergeTrees(root1, root2 *TreeNode) *TreeNode {
+	if root1 == nil && root2 == nil {
+		return nil
+	}
+
+	if root1 == nil {
+		return root2
+	}
+
+	if root2 == nil {
+		return root1
+	}
+
+	root1.Val += root2.Val
+	root1.Left = MergeTrees(root1.Left, root2.Left)
+	root1.Right = MergeTrees(root1.Right, root2.Right)
+
+	return root1
+}
